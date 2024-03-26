d72403f653fdf26c5f09d3f8d7800b30e3d1b9a9
Khiro95 <126525910+Khiro95@users.noreply.github.com>
2024-03-26 13:59:12 +0100

    Fix TabItem click using mouse left-button

---
+++ AwqatSalaat/UI/Controls/TabItemEx.cs
@@ -0,0 +1,24 @@
+﻿using System.Windows.Controls;
+using System.Windows.Input;
+
+namespace AwqatSalaat.UI.Controls
+{
+    internal class TabItemEx : TabItem
+    {
+        protected override void OnMouseLeftButtonDown(MouseButtonEventArgs e)
+        {
+            base.OnMouseLeftButtonDown(e);
+
+            if (e.Handled)
+            {
+                CaptureMouse();
+            }
+        }
+
+        protected override void OnMouseLeftButtonUp(MouseButtonEventArgs e)
+        {
+            base.OnMouseLeftButtonUp(e);
+            ReleaseMouseCapture();
+        }
+    }
+}
