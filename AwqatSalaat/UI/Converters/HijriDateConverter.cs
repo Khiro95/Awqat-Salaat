b911770db1f1134f2c5afdec3e51fb6f0f957137
Khiro95 <126525910+Khiro95@users.noreply.github.com>
2024-04-19 22:16:53 +0100

    Fix Hijri date display for WPF widget

---
+++ AwqatSalaat/UI/Converters/HijriDateConverter.cs
@@ -0,0 +1,25 @@
+﻿using AwqatSalaat.Helpers;
+using System;
+using System.Globalization;
+using System.Windows.Data;
+
+namespace AwqatSalaat.UI.Converters
+{
+    class HijriDateConverter : IValueConverter
+    {
+        public object Convert(object value, Type targetType, object parameter, CultureInfo culture)
+        {
+            if (value is DateTime dateTime)
+            {
+                return HijriDateHelper.Format(dateTime, parameter as string, LocaleManager.Default.Current);
+            }
+
+            return null;
+        }
+
+        public object ConvertBack(object value, Type targetType, object parameter, CultureInfo culture)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
