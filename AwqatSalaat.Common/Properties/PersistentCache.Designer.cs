92c2371d525f04683f77d077ab0a2c3478bf8e00
Khiro95 <126525910+Khiro95@users.noreply.github.com>
2025-05-14 14:56:35 +0100

    Increase the limit of prayer time adjustment from 10 minutes to 60 minutes

--- AwqatSalaat.Common/Properties/PersistentCache.Designer.cs
+++ AwqatSalaat.Common/Properties/PersistentCache.Designer.cs
@@ -12,7 +12,7 @@ namespace AwqatSalaat.Properties {
     
     
     [global::System.Runtime.CompilerServices.CompilerGeneratedAttribute()]
-    [global::System.CodeDom.Compiler.GeneratedCodeAttribute("Microsoft.VisualStudio.Editors.SettingsDesigner.SettingsSingleFileGenerator", "17.12.0.0")]
+    [global::System.CodeDom.Compiler.GeneratedCodeAttribute("Microsoft.VisualStudio.Editors.SettingsDesigner.SettingsSingleFileGenerator", "17.13.0.0")]
     internal sealed partial class PersistentCache : global::System.Configuration.ApplicationSettingsBase {
         
         private static PersistentCache defaultInstance = ((PersistentCache)(global::System.Configuration.ApplicationSettingsBase.Synchronized(new PersistentCache())));
